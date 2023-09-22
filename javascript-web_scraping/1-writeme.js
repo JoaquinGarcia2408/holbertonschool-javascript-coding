9f4dbc7ce90ff6a58d95d470bd9281b01a130738
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-09-22 11:56:00 -0300

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const str = process.argv[3];
-fs.writeFile(filePath, str, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
   if (err) {
     console.log(err);
   }
